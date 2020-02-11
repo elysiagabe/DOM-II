73530a162b61975d0986f7a815dab0373525a361
Elysia Gabe <elysiagabe@gmail.com>
2020-02-11 08:26:09 -0800

    Adds mouseover & mouseout events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
 // Your code goes here
 
+// dblclick event 
+const mainImg = document.querySelector(".intro img");
+const headerText = document.querySelector (".intro h2");
+
+mainImg.addEventListener('dblclick', event => {
+    mainImg.src = "img/fun-bus-alt.jpg";
+    headerText.textContent = "Where will you go?";
+})
+
+// mouseover event
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach(link => {
+    link.addEventListener('mouseover', event => {
+        link.style.color = "#17A2B8";
+        link.style.textDecoration = "underline";
+    })
+})
+
+// mouseout event 
+links.forEach(link => {
+    link.addEventListener('mouseout', event => {
+        link.style.color = "#212529";
+        link.style.textDecoration = "none";
+    })
+})
+
 // mouseenter event
 const destination = document.querySelector(".content-destination");
 destination.style.padding = "16px";
@@ -15,11 +42,4 @@ destination.addEventListener('mouseleave', event => {
     destination.style.color = "#212529";
 })
 
-// dblclick event 
-const mainImg = document.querySelector(".intro img");
-const headerText = document.querySelector (".intro h2");
 
-mainImg.addEventListener('dblclick', event => {
-    mainImg.src = "img/fun-bus-alt.jpg";
-    headerText.textContent = "Where will you go?";
-})
