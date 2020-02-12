aa6b0106caa085ecc9334568092b72c06904d5c6
Elysia Gabe <elysiagabe@gmail.com>
2020-02-11 22:17:00 -0800

    Completes stretch assignment

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -8,11 +8,27 @@ singleBlock.forEach(block => {
     })
 })
 
-let isMoving = false;
+let moveRightInterval;
+let movingBlock;
 
 singleBlock.forEach(block => {
     block.addEventListener("mousedown", event => {
-        block.style.backgroundColor = "orange";
-    
+        let leftMargin = 10;
+        movingBlock = block;
+        moveRightInterval = setInterval(move, 1);
+        function move() {
+            if (leftMargin == 500) {
+                clearInterval(moveRightInterval);
+                movingBlock.style.marginLeft = "10px";
+            } else {
+                leftMargin++;
+                movingBlock.style.marginLeft = leftMargin + "px";
+            }
+        }
     })
+})
+
+blocksParent.addEventListener("mouseup", event => {
+    clearInterval(moveRightInterval);
+    movingBlock.style.marginLeft = "10px";
 })
